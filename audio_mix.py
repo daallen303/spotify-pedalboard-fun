d1a93a53c9c4b796cc2987f96408aed2b3097b24
Daniel Allen <daniel.allen@centrapay.com>
2021-10-14 16:59:59 -0600

    Refactor adding options to click command, in order to extract the definition of options

--- audio_mix.py
+++ audio_mix.py
@@ -15,15 +15,20 @@ def hello(name):
 
 
 chorus = Chorus()
-@click.command()
-@click.option("--rate_hz", type=float, default=chorus.rate_hz)
-@click.option("--depth", type=float, default=chorus.depth)
-@click.option("--centre_delay_ms", type=float, default=chorus.centre_delay_ms)
-@click.option("--feedback", type=float, default=chorus.feedback)
-@click.option("--mix", type=float, default=chorus.mix, help="value must be between 0.0 and 1.0")
-@click.option("--input", type=str, help="input file path")
-@click.option("--output", type=str, help="output file path")
-def addchorus(rate_hz, depth, centre_delay_ms, feedback, mix, input, output):
+options = [
+  { 'name': "rate_hz", 'type': float, 'default': chorus.rate_hz, },
+  { 'name': "depth", 'type': float, 'default': chorus.depth, },
+  { 'name': "centre_delay_ms", 'type': float, 'default': chorus.centre_delay_ms, },
+  { 'name': "feedback", 'type': float, 'default': chorus.feedback, },
+  { 'name': "mix", 'type': float, 'default': chorus.mix, 'help': "value must be between 0.0 and 1.0" }
+  ]
+
+click_options = []
+for option in options:
+    help = option['help'] if 'help' in option.keys() else None
+    click_options.append(click.Option(["--" + option['name']], type=option['type'], default=option['default'], help=help))
+
+def addchorus_callback(rate_hz, depth, centre_delay_ms, feedback, mix, input, output):
   audio, sample_rate = sf.read(input)
   pedalboard = Pedalboard(
       [
@@ -43,8 +48,13 @@ def addchorus(rate_hz, depth, centre_delay_ms, feedback, mix, input, output):
   ) as f:
       f.write(effected)
 
+addchorus = click.Command('addChorus', callback=addchorus_callback, params=click_options)
+
 cli.add_command(hello)
 cli.add_command(addchorus)
 
 def main():
   cli()
+
+def get_version():
+  return 1
